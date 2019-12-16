37733fa4c1385913232104381fe0b95efa683b48
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-16 14:53:08 +0000

    Refactor sign up feature test

--- spec/back_end/features/sign_up_spec.rb
+++ spec/back_end/features/sign_up_spec.rb
@@ -2,10 +2,10 @@ feature 'sign up' do
   scenario 'a user can sign up' do
     visit '/'
     click_on 'Sign Up'
-    expect(page.has_field? 'First Name').to be true
-    expect(page.has_field? 'Surname').to be true
-    expect(page.has_field? 'Email').to be true
-    expect(page.has_field? 'Password').to be true
-    expect(page.has_field? 'Confirm Password').to be true
+    page.has_field? 'First Name'
+    page.has_field? 'Surname'
+    page.has_field? 'Email'
+    page.has_field? 'Password'
+    page.has_field? 'Confirm Password'
   end
 end
